ef439e5fe79b4519d72accf6ef5ac3047a189627
rainmen747 <jwkim9704@gmail.com>
2021-08-01 10:05:01 +0900

    pyusb_utx50_1

--- draw_with_pyusb.py
+++ draw_with_pyusb.py
@@ -21,15 +21,15 @@ window=tkinter.Tk()
 window.title("Rainmen")
 window.geometry("1000x480+100+100")
 window.resizable(True,True)
-myCanvas = tkinter.Canvas(window, bg="white", height=400, width=520)
+myCanvas = tkinter.Canvas(window, bg="white", height=400, width=1020)
 myCanvas.pack() 
 
 x=0
  
 while True:
     data= dev.read(0x81,1024*16,10)
-    for ix in range(1,200,1):
-        myCanvas.create_line(ix,20+(data[ix*2+3000]),ix+1,20+(data[ix*2+1+3000]),fill="Blue")
+    for ix in range(1,500,1):
+        myCanvas.create_line(ix*2,20+(data[ix+2000]),ix*2+2,20+(data[ix+1+2000]),fill="Blue")
     myCanvas.update()
     time.sleep(0.001)
     myCanvas.delete("all")
